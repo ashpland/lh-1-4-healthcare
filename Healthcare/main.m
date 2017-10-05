0e3d48e575813c28f8724fc42c9b4f84061f854b
Andrew Shopland <hi@ashp.land>
2017-10-05 13:53:37 -0700

    Initial Commit

---
+++ Healthcare/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Healthcare
+//
+//  Created by Andrew on 2017-10-05.
+//  Copyright © 2017 Andrew. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
