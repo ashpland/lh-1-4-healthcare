e8818e5fb05e99b38f7933be05c863d32fbcf638
Andrew Shopland <hi@ashp.land>
2017-10-05 14:38:42 -0700

    added test for init and custom inits

--- Healthcare/main.m
+++ Healthcare/main.m
@@ -15,10 +15,31 @@
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
 
-        Patient *testPatient = [Patient new];
+        Patient *testPatient1 = [Patient new];
+        Doctor *testDoctor1 = [Doctor new];
         
-        NSLog(@"%d", testPatient.age);
-    
+        NSLog(@"Patient %@ is %d years old", testPatient1.name, testPatient1.age);
+        
+        NSLog(@"%@ has a specialization in %@", testDoctor1.name, testDoctor1.specialization);
+        
+        
+        NSDateComponents *comps = [[NSDateComponents alloc] init];
+        [comps setDay:21];
+        [comps setMonth:03];
+        [comps setYear:1988];
+        NSDate *date = [[NSCalendar currentCalendar] dateFromComponents:comps];
+        
+        
+        
+        Patient *testPatient2 = [[Patient alloc] initWithName:@"Drew" andBirthDate:date];
+        Doctor *testDoctor2 = [[Doctor alloc] initWithName:@"Dr. Rutherford" andSpecialization:@"Gay Men's Health"];
+        
+        NSLog(@"Patient %@ is %d years old", testPatient2.name, testPatient2.age);
+        
+        NSLog(@"%@ has a specialization in %@", testDoctor2.name, testDoctor2.specialization);
+        
+
+
     }
     return 0;
 }
